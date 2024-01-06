9a5f3ede83cb136472d090fee38c8c1e4c913cf4
Your Name <you@example.com>
2024-01-06 13:32:13 +0530

    second commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast Food Pizza Co.</Link>
+    </header>
+  );
+}
+
+export default Header;
