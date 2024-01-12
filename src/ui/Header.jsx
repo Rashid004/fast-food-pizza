3359529d815418f56941956a8fb81a24b8269ccd
Your Name <you@example.com>
2024-01-12 21:05:05 +0530

    cartSlice

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,7 @@
 /** @format */
-
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
-// import UserName from '../features/user/UserName';
+import UserName from '../features/user/UserName';
 function Header() {
   return (
     <header className="flex justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
@@ -11,7 +10,7 @@ function Header() {
       </Link>
       <SearchOrder />
 
-      {/* <UserName /> */}
+      <UserName />
     </header>
   );
 }
