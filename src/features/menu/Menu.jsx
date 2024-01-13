305fe9033e0335a1dfab16c25931de9d589a07b1
Your Name <you@example.com>
2024-01-13 16:34:22 +0530

    Clear Cart handle

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -2,6 +2,7 @@
 import { useLoaderData } from 'react-router-dom';
 import { getMenu } from '../../services/apiRestaurant';
 import MenuItem from './MenuItem';
+
 function Menu() {
   const menu = useLoaderData();
   console.log(menu);
